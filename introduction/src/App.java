f4ed3fc4668c5b54eda2abe4fbf98a03787b31ce
idorocodes <idoroyen33@gmail.com>
2025-07-26 15:38:19 +0100

    Completed the introduction to java

--- introduction/src/App.java
+++ introduction/src/App.java
@@ -1,6 +1,12 @@
 public class App{
-    public static void main (String[] args) {
-        System.out.println("Khairat has a boyfriend and she doesn't wants us to know");
-        
+
+    public static void main (String[] args){
+        System.out.println("Hello world!");
+        System.out.println("I just said hello world!");
+
+        System.out.println("My favorite book");
+        System.out.println("My favorite book is let us c!");
+        System.out.println("That's all, thanks!");
     }
+    
 }
